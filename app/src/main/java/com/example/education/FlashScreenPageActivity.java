d3c8bbcdbbf3bbf8b2f5d5a7c96f5681b3f18620
unknown <cc.wang@shunwang.com>
2019-12-04 20:40:22 +0800

    dsdsa

--- app/src/main/java/com/example/education/FlashScreenPageActivity.java
+++ app/src/main/java/com/example/education/FlashScreenPageActivity.java
@@ -44,9 +44,9 @@ public class FlashScreenPageActivity extends BaseActivity {
     @Override
     protected void realCreate() {
         String pic = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567702323229&di=9b5a06ad29826896703020479e7a5593&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201703%2F30%2F20170330153834_K3uHf.jpeg";
-        Glide.with(this).load(pic).into(mLogo);
+//        Glide.with(this).load(pic).into(mLogo);
         Map<String, RouterBean> routerMap = WeRouter.getRouterMap();
-        Log.e("WANG", "FlashScreenPageActivity.realCreate." + routerMap.size());
+//        Log.e("WANG", "FlashScreenPageActivity.realCreate." + routerMap.size());
         webView = findViewById(R.id.webView);
         findViewById(R.id.btn).setOnClickListener(new OnFilterClickListener() {
             @Override
---
+++ app/src/main/java/com/example/education/tree/FRImageLoad.java
@@ -0,0 +1,176 @@
+package com.example.education.tree;
+
+
+/**
+ * Created by jack on 2017/8/22
+ */
+
+public class FRImageLoad {
+
+    private FRImageLoad() {
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //加载图片地址
+   /* public static void loadImage(String imageUrl, ImageView imageView) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageUrl).into(imageView);
+        }
+    }
+
+    //加载图片地址(旋转)
+    public static void loadImageRotateTransForm(String imageUrl, ImageView imageView, float rotate) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageUrl).transform(new RotateTransformation(rotate)).into(imageView);
+        }
+    }
+
+    //加载图片地址(圆角)
+    public static void loadImageRoundedTransForm(String imageUrl, ImageView imageView, int rotate) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageUrl).transform(new RoundedTransformation(rotate)).into(imageView);
+        }
+    }
+
+    //加载图片地址(圆角)
+    public static void loadImageRoundedTransForm(int res, ImageView imageView, int rotate) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(res).transform(new RoundedTransformation(rotate)).into(imageView);
+        }
+    }
+
+    //加载图片地址(去掉动画)
+    public static void loadImageDoNotAnim(String imageUrl, ImageView imageView) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageUrl).dontAnimate().into(imageView);
+        }
+    }
+
+    //加载图片地址(带占位图)
+    public static void loadImage(String imageUrl, ImageView imageView, int placeholder) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageUrl).placeholder(placeholder).error(placeholder).into(imageView);
+        }
+    }
+
+    //加载本地图片
+    public static void loadImage(Integer imageRes, ImageView imageView) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageRes).into(imageView);
+        }
+    }
+
+    //加载图片Uri
+    public static void loadImage(Uri imageUri, ImageView imageView) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageUri).into(imageView);
+        }
+    }
+
+    //加载图片文件
+    public static void loadImage(File imageFile, ImageView imageView) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageFile).into(imageView);
+        }
+    }
+
+    //加载图片字节
+    public static void loadImage(byte[] imageByte, ImageView imageView) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageByte).into(imageView);
+        }
+    }
+
+    //加载Gif图片地址
+    public static void loadImageGif(String imageUrl, ImageView imageView) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).asGif().load(imageUrl).into(imageView);
+        }
+    }
+
+    //加载本地Gif图片地址
+    public static void loadImageGif(@DrawableRes int drawableRes, ImageView imageView) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).asGif().load(drawableRes).diskCacheStrategy(DiskCacheStrategy.NONE).into(imageView);
+        }
+    }
+
+    //监听图片加载
+    public static void loadImage(String imageUrl, ImageView imageView, RequestListener<Drawable> requestListener) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageUrl).listener(requestListener).into(imageView);
+        }
+    }
+
+    *//**
+     * 加载图片不带缓存
+     *
+     * @param imageUrl
+     * @param imageView
+     *//*
+    public static void loadImageNoCache(String imageUrl, ImageView imageView) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageUrl).skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE).into(imageView);
+        }
+    }
+
+    public static void loadCircle(String url, ImageView imageView, int placeholder) {
+        GlideApp.with(imageView.getContext())
+                .load(url)
+                .placeholder(placeholder)
+                .error(placeholder)
+                .transform(new RoundTransformation())
+                .into(imageView);
+    }
+
+    public static void loadCircle(int src, ImageView imageView, int placeholder) {
+        GlideApp.with(imageView.getContext())
+                .load(src)
+                .placeholder(placeholder)
+                .error(placeholder)
+                .transform(new RoundTransformation())
+                .into(imageView);
+    }
+
+    public static void loadCircle(int src, ImageView imageView) {
+        GlideApp.with(imageView.getContext())
+                .load(src)
+                .transform(new RoundTransformation())
+                .into(imageView);
+    }
+
+    public static void loadCircle(String url, ImageView imageView) {
+        GlideApp.with(imageView.getContext())
+                .load(url)
+                .transform(new RoundTransformation())
+                .into(imageView);
+    }
+
+    *//**
+     * 将图片按照自定义的转换器进行转换
+     *
+     * @param imageUrl       图片地址
+     * @param imageView      填充控件
+     * @param transformation 自定义转换器
+     *//*
+    public static void loadImageTransForm(String imageUrl, ImageView imageView, Transformation<Bitmap> transformation) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageUrl).transform(transformation).into(imageView);
+        }
+    }
+
+    *//**
+     * 将图片按照自定义的转换器进行转换
+     *
+     * @param imageSrc       图片地址
+     * @param imageView      填充控件
+     * @param transformation 自定义转换器
+     *//*
+    public static void loadImageTransForm(int imageSrc, ImageView imageView, Transformation<Bitmap> transformation) {
+        if (Util.isOnMainThread()) {
+            GlideApp.with(imageView.getContext()).load(imageSrc).transform(transformation).into(imageView);
+        }
+    }*/
+}
